4e6cc15c2d70130c7ce69b0a1943630105ca61f1
Chaudhary Hamdan <54744843+hamdan-codes@users.noreply.github.com>
2021-09-02 14:43:18 +0530

    Lab 7 Push

---
+++ Lab 7/check.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<sys/time.h>
+#define STDIN 0 // file descriptor for standard input
+int main(void) {
+    struct timeval tv;
+    fd_set readfds;
+    tv.tv_sec = 2;
+    tv.tv_usec = 500000;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN, &readfds);
+    // don’t care about writefds and exceptfds:
+    select(STDIN+1, &readfds, NULL, NULL, &tv);
+    if (FD_ISSET(STDIN, &readfds))
+        printf("A key was pressed!\n");
+    else
+        printf("Timed out.\n");
+    return 0;
+}
+
